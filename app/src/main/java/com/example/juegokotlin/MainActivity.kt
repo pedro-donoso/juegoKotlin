0340e00c09747344beaf9764f584a1396b17ee70
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-26 16:05:33 -0400

    creo funciones en MainActivity

--- app/src/main/java/com/example/juegokotlin/MainActivity.kt
+++ app/src/main/java/com/example/juegokotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.juegokotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.juegokotlin.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +10,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
 
     }
 }
