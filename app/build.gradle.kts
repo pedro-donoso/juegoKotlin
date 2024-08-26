edeaedb5185ee0093c675ee2136642d1ae1881f4
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-26 15:44:39 -0400

    configuro gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,10 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
+    }
+    dataBinding {
+        enable = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -50,6 +54,13 @@ android {
 }
 
 dependencies {
+
+    val nav_version = "2.7.7"
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    implementation ("com.airbnb.android:lottie:3.7.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
