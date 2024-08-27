e329da9825889cbf0b1dfec15e045187b22a9074
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-27 14:07:55 -0400

    comento CounterViewModel

--- app/src/main/java/com/example/juegokotlin/CounterViewModel.kt
+++ app/src/main/java/com/example/juegokotlin/CounterViewModel.kt
@@ -1,20 +1,26 @@
 package com.example.juegokotlin
 
+//importaciones
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+//clase CounterViewModel que hereda de ViewModel
 class CounterViewModel: ViewModel() {
+
+    //variables para almacenar los contadores y el ganador
     private val _counter1 = MutableLiveData(10)
+    //propiedad para observar los cambios en el contador 1
     val counter1 = _counter1
 
     private val _counter2 = MutableLiveData(10)
+    //propiedad para observar los cambios en el contador 2
     val counter2 = _counter2
 
-    //Winner
-    private val _winner = MutableLiveData<String>(null)
+    //variable para almacenar el ganador
+    private val _winner = MutableLiveData<String?>(null)
     val winner = _winner
 
-    //Functions
+    //Funcion para comprobar el ganador
     private fun checkWinner(){
         if(_counter1.value == 20){
             _winner.value = "Player 1"
@@ -23,7 +29,7 @@ class CounterViewModel: ViewModel() {
         }
     }
 
-    //Increment and decrement
+    //funciones para incrementar y decrementar los contadores
     fun incrementCounter1(){
         _counter1.value = (_counter1.value ?: 10) + 1
         checkWinner()
@@ -44,7 +50,7 @@ class CounterViewModel: ViewModel() {
         checkWinner()
     }
 
-    //Reset
+    //funcion para reiniciar los contadores y el ganador
     fun resetCounters(){
         _counter1.value = 10
         _counter2.value = 10
