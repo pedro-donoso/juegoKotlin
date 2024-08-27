954ffc56ff77501dfe4db243c269fd01bccce04c
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-27 13:16:49 -0400

    agrego boton animacion

--- app/src/main/java/com/example/juegokotlin/MainActivity.kt
+++ app/src/main/java/com/example/juegokotlin/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.juegokotlin
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.juegokotlin.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: MainActivityBinding
+    private val viewModel: CounterViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +18,20 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragmentContainer1, Player1Fragment())
             .replace(R.id.fragmentContainer2, Player2Fragment())
             .commit()
+
+        viewModel.winner.observe(this) { winner ->
+            winner?.let {
+                showWinnerAnimation(it)
+            }
+        }
+
+        binding.restartButton.setOnClickListener {
+            restartGame()
+        }
     }
 
     private fun showWinnerAnimation(winner: String) {
-        // binding.winnerTextView.text = "El ganador es: $winner"
+        //binding.winnerTextView.text = "El ganador es: $winner"
         binding.animationContainer.visibility = View.VISIBLE
         binding.winnerAnimation.playAnimation()
         disableButtons()
@@ -33,6 +44,25 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.fragmentContainer2) as? Player2Fragment
         fragment1?.disableButton()
         fragment2?.disableButton()
+    }
+
+    private fun restartGame() {
+        binding.animationContainer.visibility = View.GONE
+        binding.winnerAnimation.cancelAnimation()
+        viewModel.resetCounters()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer1, Player1Fragment())
+            .replace(R.id.fragmentContainer2, Player2Fragment())
+            .commit()
+        viewModel.winner.observe(this) { winner ->
+            winner?.let {
+                println("value winner "+it)
+                showWinnerAnimation(it)
+            }
+        }
+        binding.restartButton.setOnClickListener {
+            restartGame()
+        }
     }
 
     override fun onDestroy() {
