f0850e65d2e323f33ea13f10d79b5156e5a2b735
pedro-donoso <pedro.donoso.r@usach.cl>
2024-08-27 10:49:36 -0400

    creo incremento y decremento

--- app/src/main/java/com/example/juegokotlin/CounterViewmodel.kt
+++ app/src/main/java/com/example/juegokotlin/CounterViewmodel.kt
@@ -12,4 +12,33 @@ class CounterViewmodel: ViewModel() {
 
     private val _winner = MutableLiveData<String>(null)
     val winner = _winner
+
+    private fun checkWinner(){
+        if(_counter1.value == 20){
+            _winner.value = "Player 1"
+        }else if(_counter2.value == 20){
+            _winner.value = "Player 2"
+        }
+    }
+
+    fun incrementCounter1(){
+        _counter1.value = (_counter1.value ?: 10) + 1
+        checkWinner()
+    }
+
+    fun incrementCounter2(){
+        _counter2.value = (_counter2.value ?: 10) + 1
+        checkWinner()
+    }
+
+    fun decrementCounter1(){
+        _counter1.value = (_counter1.value ?: 10) - 1
+        checkWinner()
+    }
+
+    fun decrementCounter2(){
+        _counter2.value = (_counter2.value ?: 10) - 1
+        checkWinner()
+    }
+
 }
